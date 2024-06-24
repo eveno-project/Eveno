12005ddc4b6d05771d5c857b73772dba95604c56
Théodore DUCHEMIN <theodoreduchemin@gmail.com>
2024-06-24 13:41:30 +0200

    save before Rebase

--- src/app/event/remove/[id]/page.tsx
+++ src/app/event/remove/[id]/page.tsx
@@ -4,9 +4,9 @@ import { getById } from "@services/event";
 
 export default async function Page({ params }: { params: { id: number } }) {
     const event = await getById((params.id));
-    const updateEventWithId = updateEvent.bind(null, +params.id);
-
-    return (
-        <EventForm action={updateEventWithId} event={event} />
-    );
+    if (event) {
+        return (<p>Event Found</p>);
+    } else {
+        return (<p>No event Found</p>);
+    }
 };
