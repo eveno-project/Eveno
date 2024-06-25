aafb05cfb5f5ee9f41b648eacf8b9fff4a7ff74a
Théodore DUCHEMIN <theodoreduchemin@gmail.com>
2024-06-25 10:38:22 +0200

    fix : j ai retirer les console log

--- src/app/event/userEvent/page.tsx
+++ src/app/event/userEvent/page.tsx
@@ -3,7 +3,6 @@ import { getByUserEmail } from "@services/event";
 
 export default async function Page() {
     const events = await getByUserEmail('user1@gmail.com');
-    console.log(events);
     return (
         <div>
             <EventList events={events} />
