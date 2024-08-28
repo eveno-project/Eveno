933357018cda6ef9892a677b80fcdc4e40bb05d6
N. Breant <nbreant@assist-evolution.com>
2024-08-28 12:07:23 +0200

    fix: security api

--- src/app/api/users/delete-account/route.ts
+++ src/app/api/users/delete-account/route.ts
@@ -1,8 +1,15 @@
 import prisma from "@utils/db";
 import { NextRequest, NextResponse } from "next/server";
 import { compare } from "bcryptjs";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@lib/auth";
 
 export async function DELETE(req: NextRequest) {
+	const session = await getServerSession(authOptions);
+	if (!session || !session.user) {
+		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+	}
+
 	if (req.method !== "DELETE") {
 		return NextResponse.json({ message: 'Méthode non autorisée' }, { status: 405 });
 	}
