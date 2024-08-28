f67011f5c26af03665ac9405362c15f546aeba90
N. Breant <nbreant@assist-evolution.com>
2024-08-28 22:46:26 +0200

    fix: delete api event

--- src/app/api/event/[id]/route.ts
+++ src/app/api/event/[id]/route.ts
@@ -1,34 +1,75 @@
-import { NextResponse } from 'next/server';
-import { create, getAll, update } from '@services/event';
-import { eventSchema, updateEventSchema } from '@validators/event.schema';
-import { ZodIssue } from 'zod';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@lib/auth';
+import { NextResponse } from "next/server";
+import { deleteOne, getById, update } from "@services/event";
+import { authOptions } from "@lib/auth";
+import { getServerSession } from "next-auth";
+import { Role } from "@constants/role";
 
+export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+    const parsedId = parseInt(params.id);
+    const session = await getServerSession(authOptions);
 
-export async function GET() {
     try {
-        const events = await getAll();
-        return NextResponse.json({ data: events }, { status: 200 });
+        if (session && session.user) {
+            // verify that the user is the owner of the event
+            const event = await getById(parsedId);
+
+            if (event?.user.id !== session.user.id) {
+                return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+            }
+
+            const success = await deleteOne(parsedId, session.user.id);
+
+            if (success) {
+                // Return success response
+                return NextResponse.json({ message: "Event deleted successfully" }, { status: 200 });
+            } else {
+                // Return failure response if deletion was unsuccessful
+                return NextResponse.json({ error: "Failed to delete event" }, { status: 500 });
+            }
+        } else {
+            // Return unauthorized response if user is not authenticated
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
     } catch (error) {
-        console.error({ error });
-        throw NextResponse.json({ error }, { status: 500 });
+        // Return error response
+        return NextResponse.json({ error: "An error occurred while deleting the event" }, { status: 500 });
+    }
+}
+
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+        }
+
+        if (session.user.role !== Role.ADMIN) {
+            return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+        }
+
+        const parsedId = parseInt(params.id);
+        const event = await getById(parsedId);
+        return NextResponse.json({ data: event }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: "An error occurred while fetching the event" }, { status: 500 });
     }
 }
 
 
-export async function POST(req: Request) {
-    // Récupérer la session utilisateur
+
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
     const session = await getServerSession(authOptions);
     const userId = session?.user.id;
 
-    // Vérifier si l'utilisateur est authentifié
     if (!userId) {
         return NextResponse.redirect('/');
     }
 
     try {
         const data = await req.json();
+
+        data.id = parseInt(params.id);
 
         const { localizations } = data;
 
@@ -56,8 +97,7 @@ export async function POST(req: Request) {
 
         const { adult, ...result } = data;
 
-        const event = await create({ ...result, adult: !adult });
-
+        const event = await update({...result, adult: !adult });
         return NextResponse.json({ success: event });
 
     } catch (error) {
@@ -65,4 +105,3 @@ export async function POST(req: Request) {
         return NextResponse.json({ error: 'Erreur lors du traitement des données.' }, { status: 500 });
     }
 }
-
