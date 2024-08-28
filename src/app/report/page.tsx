5b19378bacb246a7b95ad0681ec361c2b73c1f34
ArbeitsTheo <92360574+ArbeitsTheo@users.noreply.github.com>
2024-08-28 13:51:14 +0200

    Feature/ticket auto (#83)
    * feature create issue
    
    * add label bug
    
    * rebase develop
    and fix label name

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,11 +1,16 @@
 "use client";
+
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { TextField, Button, Box, FormHelperText, FormControl } from '@mui/material';
+import { TextField, Box, FormControl, Container, Typography } from '@mui/material';
 import { IssueSchema, IssueValues } from '@validators/issue.shema';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useRouter } from 'next/navigation';
 
-export default function IssueForm() {
+export default function Page() {
+    const router = useRouter();
+
     const {
         control,
         handleSubmit,
@@ -28,70 +33,65 @@ export default function IssueForm() {
                 body: JSON.stringify(issueValues),
             });
 
-            if (response.ok) {
-                alert('Ticket créée avec succès');
-            } else {
+            if (!response.ok) {
                 console.error('Erreur lors de la création de l\'issue');
-                alert('Une erreur est survenue lors de la création de l\'issue.');
+            } else {
+                router.push("/");
             }
         } catch (error) {
             console.error('Une erreur est survenue:', error);
-            alert('Une erreur est survenue lors de la création de l\'issue.');
         }
     };
 
     return (
-        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-            <FormControl fullWidth margin="normal" error={!!errors.title}>
-                <Controller
-                    name="title"
-                    control={control}
-                    render={({ field }) => (
-                        <TextField
-                            {...field}
-                            label="Titre"
-                            variant="outlined"
-                            fullWidth
-                            error={!!errors.title}
-                            helperText={errors.title?.message}
-                        />
-                    )}
-                />
-            </FormControl>
-            <FormControl fullWidth margin="normal" error={!!errors.body}>
-                <Controller
-                    name="body"
-                    control={control}
-                    render={({ field }) => (
-                        <TextField
-                            {...field}
-                            label="Description"
-                            variant="outlined"
-                            multiline
-                            rows={4}
-                            fullWidth
-                            error={!!errors.body}
-                            helperText={errors.body?.message}
-                        />
-                    )}
-                />
-            </FormControl>
+        <Container component="main" maxWidth="md">
+            <Typography variant='h6'>Reporter un incident</Typography>
+            <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                <FormControl fullWidth margin="normal" error={!!errors.title}>
+                    <Controller
+                        name="title"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                label="Titre"
+                                variant="outlined"
+                                fullWidth
+                                error={!!errors.title}
+                                helperText={errors.title?.message}
+                            />
+                        )}
+                    />
+                </FormControl>
+                <FormControl fullWidth margin="normal" error={!!errors.body}>
+                    <Controller
+                        name="body"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                label="Description"
+                                variant="outlined"
+                                multiline
+                                rows={4}
+                                fullWidth
+                                error={!!errors.body}
+                                helperText={errors.body?.message}
+                            />
+                        )}
+                    />
+                </FormControl>
 
-            <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                disabled={isSubmitting}
-            >
-                Créer un Ticket
-            </Button>
-            {errors && Object.keys(errors).length > 0 && (
-                <Box sx={{ marginTop: '1rem', color: 'red' }}>
-                    {Object.values(errors).map((error, index) => (
-                        <FormHelperText key={index}>{error.message}</FormHelperText>
-                    ))}
-                </Box>
-            )}
-        </Box>
+                <LoadingButton
+                    loading={isSubmitting}
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={isSubmitting}
+                >
+                    Créer un Ticket
+                </LoadingButton>
+            </Box>
+        </Container>
     );
 }
